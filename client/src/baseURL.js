c3625957bea9864dff1c3efae769f059023b9126
Shaun Valentine <valensh1@yahoo.com>
2023-08-11 19:13:37 -0700

    Latest code trying to deploy to Azure

--- client/src/baseURL.js
+++ client/src/baseURL.js
@@ -1,3 +1,4 @@
-const baseURL = 'https://youth-sports-gamerz.azurewebsites.net';
+// const baseURL = 'https://youth-sports-gamerz.azurewebsites.net';
+const baseURL = 'http://localhost:3000';
 
 export default baseURL;
