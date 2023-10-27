2edc306917543177f5a7db7fefaba5164daf2327
Shaun Valentine <valensh1@yahoo.com>
2023-10-27 14:45:02 -0700

    Latest new player form

--- client/src/views/Hockey/AddPlayer.js
+++ client/src/views/Hockey/AddPlayer.js
@@ -7,6 +7,7 @@ import Form from 'react-bootstrap/Form';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Navbar from '../../components/Navbar';
 
 function AddPlayer() {
   const submitForm = (event) => {
@@ -15,113 +16,157 @@ function AddPlayer() {
   };
 
   return (
-    <Form onSubmit={submitForm}>
-      <Form.Group as={Col} controlId="formGridFirstName">
-        <FloatingLabel
-          controlId="floatingInput"
-          label="First Name"
-          className="mb-3"
-        >
-          <Form.Control placeholder="First Name" />
-        </FloatingLabel>
-      </Form.Group>
+    <>
+      <Navbar />
+      <Form onSubmit={submitForm} id="add-player">
+        <div id="form-heading">
+          <h1>Add New Player</h1>
+        </div>
+        <Row>
+          <Col>
+            <Form.Group
+              controlId="formGridFirstName"
+              id="first-name-input-box"
+              className="form-input-box"
+            >
+              <FloatingLabel
+                controlId="floatingInput"
+                label="First Name"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="First Name"
+                  id="first-name-input"
+                  className="form-inputs"
+                />
+              </FloatingLabel>
+            </Form.Group>
+          </Col>
 
-      <Form.Group as={Col} controlId="formGridLastName">
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Last Name"
-          className="mb-3"
-        >
-          <Form.Control placeholder="Last Name" />
-        </FloatingLabel>
-      </Form.Group>
+          <Col>
+            <Form.Group controlId="formGridLastName" className="form-input-box">
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Last Name"
+                className="mb-3"
+              >
+                <Form.Control placeholder="Last Name" className="form-inputs" />
+              </FloatingLabel>
+            </Form.Group>
+          </Col>
+        </Row>
 
-      <Form.Group as={Col} controlId="formGridDateOfBirth">
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Date of Birth"
-          className="mb-3"
-        >
-          <Form.Control placeholder="Date of Birth" />
-        </FloatingLabel>
-      </Form.Group>
+        <div id="form-date-container">
+          <Form.Group
+            as={Col}
+            controlId="formGridDateOfBirth"
+            className="form-input-box"
+            id="form-date-container"
+          >
+            <Form.Label id="date-label">Date of Birth</Form.Label>
+            <LocalizationProvider dateAdapter={AdapterDayjs} id="date-input">
+              <DatePicker />
+            </LocalizationProvider>
+          </Form.Group>
+        </div>
 
-      <Form.Group as={Col} controlId="formGridHeight">
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Height (Inches)"
-          className="mb-3"
-        >
-          <Form.Control placeholder="Height (Inches)" />
-        </FloatingLabel>
-      </Form.Group>
+        <Row>
+          <Col>
+            <Form.Group
+              as={Col}
+              controlId="formGridHeight"
+              className="form-input-box"
+            >
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Height (Inches)"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Height (Inches)"
+                  className="form-inputs"
+                />
+              </FloatingLabel>
+            </Form.Group>
+          </Col>
 
-      <Form.Group as={Col} controlId="formGridWeight">
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Weight (lbs)"
-          className="mb-3"
-        >
-          <Form.Control placeholder="Weight (lbs)" />
-        </FloatingLabel>
-      </Form.Group>
+          <Col>
+            <Form.Group
+              as={Col}
+              controlId="formGridWeight"
+              className="form-input-box"
+            >
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Weight (lbs)"
+                className="mb-3"
+              >
+                <Form.Control
+                  placeholder="Weight (lbs)"
+                  className="form-inputs"
+                />
+              </FloatingLabel>
+            </Form.Group>
+          </Col>
+        </Row>
 
-      <Form.Group as={Col} controlId="formGridHand">
-        <FloatingLabel controlId="floatingInput" label="Hand" className="mb-3">
-          <Form.Control placeholder="Hand" />
-        </FloatingLabel>
-      </Form.Group>
+        <Form.Group as={Col} controlId="formGridHand">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Hand"
+            className="mb-3"
+          >
+            <Form.Control placeholder="Hand" />
+          </FloatingLabel>
+        </Form.Group>
 
-      <Form.Group as={Col} controlId="formGridGender">
-        <Form.Select aria-label="gender-selection">
-          <option>Select Gender</option>
-          <option value="1">Male</option>
-          <option value="2">Female</option>
-        </Form.Select>
-      </Form.Group>
+        <Form.Group as={Col} controlId="formGridGender">
+          <Form.Select aria-label="gender-selection">
+            <option>Select Gender</option>
+            <option value="1">Male</option>
+            <option value="2">Female</option>
+          </Form.Select>
+        </Form.Group>
 
-      <Form.Group as={Col} controlId="formGridShareContact">
-        <Form.Select aria-label="gender-selection">
-          <Form.Label>Share Contact</Form.Label>
-          <option>Share Contact?</option>
-          <option value="1">Yes</option>
-          <option value="2">No</option>
-        </Form.Select>
-      </Form.Group>
+        <Form.Group as={Col} controlId="formGridShareContact">
+          <Form.Select aria-label="gender-selection">
+            <Form.Label>Share Contact</Form.Label>
+            <option>Share Contact?</option>
+            <option value="1">Yes</option>
+            <option value="2">No</option>
+          </Form.Select>
+        </Form.Group>
 
-      <Form.Group as={Col} controlId="formGridEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="name@yahoo.com" />
-      </Form.Group>
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="name@yahoo.com" />
+        </Form.Group>
 
-      <Form.Group as={Col} controlId="formGridPhoneNumber">
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Phone Number"
-          className="mb-3"
-        >
-          <Form.Control placeholder="Phone Number" />
-        </FloatingLabel>
-      </Form.Group>
+        <Form.Group as={Col} controlId="formGridPhoneNumber">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Phone Number"
+            className="mb-3"
+          >
+            <Form.Control placeholder="Phone Number" />
+          </FloatingLabel>
+        </Form.Group>
 
-      <Form.Group as={Col} controlId="formGridContactNames">
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Contact Names"
-          className="mb-3"
-        >
-          <Form.Control placeholder="Contact Names" />
-        </FloatingLabel>
-      </Form.Group>
+        <Form.Group as={Col} controlId="formGridContactNames">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Contact Names"
+            className="mb-3"
+          >
+            <Form.Control placeholder="Contact Names" />
+          </FloatingLabel>
+        </Form.Group>
 
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker />
-      </LocalizationProvider>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
   );
 }
 export default AddPlayer;
